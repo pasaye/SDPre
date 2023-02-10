925fe8b1eeaa04fb929728870c7dd87129d336fd
Pasaye <pasayejoseph@yahoo.com>
2023-02-09 23:14:52 -0600

    Objective3Lab2.class Objective3Lab2.java

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -4,7 +4,10 @@ public class Objective3Lab2 {
 		side1 = 5;
 		side2 = 6;
 		hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
-		System.out.println(hypotenuse);
+		String sent1 = "The hypotenuse of a triangle with sides ";
+		String sent2 = " and ";
+		String sent3 = " is ";
+		System.out.println(sent1 + side1 + sent2 + side2 + sent3 + hypotenuse);
 
 	}
 
