cd35cc16c2fa5234c45ab8d374b02946ec504158
Pasaye <pasayejoseph@yahoo.com>
2023-02-09 19:42:39 -0600

    all

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Bobby B";
+		int age = 100;
+		String state = "Arkansas";
+		System.out.println("Name: " + name +" "+ "Age: " + age + " " + "State: " + state);
+	}
+}
