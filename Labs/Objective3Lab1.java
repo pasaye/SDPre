b7f07cc081c2cfdfaa6b002ac21cda316c0c8369
Pasaye <pasayejoseph@yahoo.com>
2023-02-09 22:38:36 -0600

    Objective3Lab1.class Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1, num2, num3, average;
+		num1 = 3;
+		num2 = 12;
+		num3 = 32;
+		average = (num1 + num2 + num3) / 3;
+		System.out.println(average);
+
+	}
+}
