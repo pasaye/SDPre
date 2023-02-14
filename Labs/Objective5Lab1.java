0ac225942f26aa8b483079ee3a3ff045fb835fa3
Pasaye <pasayejoseph@yahoo.com>
2023-02-13 23:15:11 -0600

    Objective5Lab1.java Objective5Lab1.class

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,15 @@
+public class Objective5Lab1 {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 15;
+
+		 if(num1 > num2) {
+		 	System.out.println(" 5 is greater than 15");
+
+		 }
+		 else if(num2 > num1){
+		 	System.out.println("15 is greater than 5");
+
+		 }
+	}
+}
