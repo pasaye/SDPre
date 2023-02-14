b327c8401ddccf071cf0efc916271281a23326eb
Pasaye <pasayejoseph@yahoo.com>
2023-02-14 13:28:43 -0600

    Objective7Lab2.java Objective7Lab2.class

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,13 @@
+public class Objective7Lab2 {
+	public static void main(String[] args) {
+		int counter = 0;
+
+		while(counter <= 20) {
+			System.out.println(counter);
+			counter = counter + 1;
+			if(counter > 20) {
+				System.out.println("Phew! Too tired to keep counting.");
+			}
+		}
+	}
+}
