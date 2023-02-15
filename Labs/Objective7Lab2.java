aa09b4b8330fc34e91c1bfd481509f9f79703e52
Pasaye <pasayejoseph@yahoo.com>
2023-02-15 10:26:13 -0600

    Objective7Lab2.java Objective7Lab4.class Objective7Lab4.java

--- Labs/Objective7Lab2.java
+++ Labs/Objective7Lab2.java
@@ -1,6 +1,6 @@
 public class Objective7Lab2 {
 	public static void main(String[] args) {
-		int counter = 0;
+		int counter = 1;
 
 		while(counter <= 20) {
 			System.out.println(counter);
---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int num = 20;
+		int count = 1;
+		int sum = 0;
+		
+		while(count <= num) {
+			sum += count;
+			count++;
+		}
+		System.out.println(sum);
+		
+	}
+}
