a7ab58c14813ad3c93f70e9807b2a374db6087eb
Pasaye <pasayejoseph@yahoo.com>
2023-02-18 08:57:07 -0600

    Objective8Lab1.java

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+	public static void  main(String[] args) {
+		int i;
+		for(i = 1; i < 11; i++){
+			System.out.println(i);
+		}
+
+		for(i = 10; i >=1; i--) {
+			System.out.println(i);
+		}
+	}
+}
